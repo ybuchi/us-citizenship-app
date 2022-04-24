79e406189c7d30ce4b18e49aa7bc6704f7600477
ybuchi <yabuch_ch@hotmail.com>
2022-04-24 12:06:03 -0400

    Added filter buttons to the Study page

--- src/Components/StudyPage/StudyPage.js
+++ src/Components/StudyPage/StudyPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import questionsData from "../../Data/questions";
 import QuestionCard from "../QuestionCard/QuestionCard";
+import { Button } from "react-bootstrap";
 
 function StudyPage(){
     const mappedQuestions = questionsData.map((question)=>{
@@ -10,6 +11,19 @@ function StudyPage(){
     return(
         <>
         <h1>Study Page</h1>
+
+        <div id="study-filter">
+            <h4>Filter by Question Type</h4>
+            <Button variant="light">history</Button>
+            <Button variant="light">colonial history</Button>
+            <Button variant="light">1800s</Button>
+            <Button variant="light">recent history</Button>
+            <Button variant="light"> civcs</Button>
+            <Button variant="light">symbols and holidays</Button>
+            <Button variant="light">flagged</Button>
+        </div>
+        
+
         {mappedQuestions}
         </>
     )
