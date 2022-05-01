e04f80360672d62f72a968443a5306c1a2f331f4
ybuchi <yabuch_ch@hotmail.com>
2022-05-01 16:26:30 -0400

    Fixed the data filtering for the quiz type

--- src/Components/QuizPage/QuizPage.js
+++ src/Components/QuizPage/QuizPage.js
@@ -7,63 +7,10 @@ import { useOutletContext } from "react-router-dom";
 
 function QuizPage(){
     const [questionsData, setQuestionsData] = useOutletContext();
-    const [quizQuestions, setQuizQuestions] = useState([]);
-
-
-    //A function that filters out the questionsData to a particular question type (history, civics etc)
-    function generateThematicQuestionsArray(questionType){
-        const thematicQuestionsArray = questionsData.filter(question=>question.type.includes(questionType))
-        return thematicQuestionsArray
-    }
-
-    //A helper function to shuffle arrays
-    function shuffle(array) {
-        let m = array.length, t, i;
-      
-        // While there remain elements to shuffle…
-        while (m) {
-      
-          // Pick a remaining element…
-          i = Math.floor(Math.random() * m--);
-      
-          // And swap it with the current element.
-          t = array[m];
-          array[m] = array[i];
-          array[i] = t;
-        }
-      
-        return array;
-      }
-
-      //A function to fetch a random set of 20 questions from the questions Data
-      function fetchRandom20Questions(){
-        //Generate an array of consecutive numbers that is the length fo the Questionsarray
-        const baseArray = []
-
-        //Hard coding CIVICS CHANGE THIS!!!!!!!!!!!!!
-        const civicsQuestions = generateThematicQuestionsArray("civics");
-         console.log(civicsQuestions);
     
-        for (let i = 1; i < civicsQuestions.length; i++){
-            baseArray.push(i)
-        }
-        //Shuffle that array
-        const shuffledBase = shuffle(baseArray)
-
-        //Use the first 20 numbers in the new array
-        const base20index = shuffledBase.slice(0,20)
-        const final20questions = base20index.map(index=>civicsQuestions[index])
-        console.log("FInal20",final20questions)
-
-        setQuizQuestions(final20questions)
-
-        return(quizQuestions)
-    }
-
-  
     return(
         <>
-        <Outlet context={[questionsData, setQuestionsData, generateThematicQuestionsArray, fetchRandom20Questions, quizQuestions]}/>
+        <Outlet context={[questionsData, setQuestionsData]}/>
         </>
     )
 }
--- src/Components/TwentyQuestions/TwentyQuestions.js
+++ src/Components/TwentyQuestions/TwentyQuestions.js
@@ -5,11 +5,62 @@ import QuizQuestionCard from "../QuizQuestionCard/QuizQuestionsCard";
 
 
 function TwentyQuestions(){
-    const [quizQuestions] = useOutletContext();
+    const [questionsData] = useOutletContext();
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [quizIsFinished, setQuizIsFinished] = useState(false);
+    const [quizQuestions, setQuizQuestions] = useState([]);
 
-   console.log(quizQuestions);
+   
+
+    useEffect(()=>{
+         //A function to fetch a random set of 20 questions from the questions Data
+          //A function that filters out the questionsData to a particular question type (history, civics etc)
+        function generateThematicQuestionsArray(questionType){
+            const thematicQuestionsArray = questionsData.filter(question=>question.type.includes(questionType))
+            return thematicQuestionsArray
+        }
+
+    //A helper function to shuffle arrays
+    function shuffle(array) {
+        let m = array.length, t, i;
+      
+        // While there remain elements to shuffle…
+        while (m) {
+      
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+      
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+      
+        return array;
+      }
+        function fetchRandom20Questions(){
+            //Generate an array of consecutive numbers that is the length fo the Questionsarray
+            const baseArray = []
+
+            //Hard coding CIVICS CHANGE THIS!!!!!!!!!!!!!
+            const civicsQuestions = generateThematicQuestionsArray("civics");
+            console.log(civicsQuestions);
+        
+            for (let i = 1; i < civicsQuestions.length; i++){
+                baseArray.push(i)
+            }
+            //Shuffle that array
+            const shuffledBase = shuffle(baseArray)
+
+            //Use the first 20 numbers in the new array
+            const base20index = shuffledBase.slice(0,20)
+            const final20questions = base20index.map(index=>civicsQuestions[index])
+
+            return(final20questions)
+    }
+        setQuizQuestions(fetchRandom20Questions())
+
+    }, [questionsData])
     return(
         <>
         <div id="twenty-questions-title">
