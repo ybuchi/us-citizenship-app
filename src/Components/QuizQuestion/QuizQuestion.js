89f7a6d95a85b674e84a77f08e40426ce4355e7e
ybuchi <yabuch_ch@hotmail.com>
2022-05-01 20:25:59 -0400

    Still no funcational answers input. Need to refactor the code. THis may involve moving the generatePossibleAnswer to a parent component so that the question answers order do not reorder themselves everytime an input is selcted

--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -4,20 +4,56 @@ import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form';
 
 function QuizQuestion({ question, handleNextQuestion, answer, setAnswer}){
-    
-    function handleTypingChange(e){
-        const current_value = e.target.value;
+    //Randomize a correct answer
+    function generatePossibleAnswer(answersArray){
+        const arrayIndex = Math.floor((Math.random() * answersArray.length))
+        return answersArray[arrayIndex]
+    }
+    const randomPossibleAnswer = generatePossibleAnswer(question.answers)
+    //Join the correct answer into the array of incorrect answers to display them
+    const answerOptionsArray = question.wrongAnswers.concat(randomPossibleAnswer) 
+    const randomizedOptionsArray = shuffle(answerOptionsArray);
+
+    const mappedOptions = randomizedOptionsArray.map(option => {
+        return <Form.Check type="checkbox" name="answer" value = {answer} onChange={handleChoiceChange} label={`${option}`}/>
+    })
+
+    function shuffle(array) {
+        let m = array.length, t, i;
+      
+        // While there remain elements to shuffle…
+        while (m) {
+      
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+      
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+      
+        return array;
+      }
+
+    function handleChoiceChange(e){
+        const current_value = e.target;
+        console.log("Target Value", current_value);
         setAnswer(current_value);
     }
+
    
     return(
         <>
         <header className="quiz-question-title">{question.question}</header>
         <hr></hr>
         <Form onSubmit={handleNextQuestion}>
-            <Form.Label>Answer:</Form.Label>
-            <Form.Control type="text" name="answer" value={answer} onChange={handleTypingChange} placeholder="Your answer here"></Form.Control>
-            <Button type="submit">Submit</Button>
+            <Form.Check onChange = {handleChoiceChange}>
+            {mappedOptions}
+            </Form.Check>
+{/* [            <Form.Control type="text" name="answer" value={answer} onChange={handleTypingChange} placeholder="Your answer here"></Form.Control> */}
+                
+]            <Button type="submit">Submit</Button>
         </Form>
         
         </>
