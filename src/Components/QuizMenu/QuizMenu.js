b04197719769a0b7fabba9247cad46e5879ff2fb
ybuchi <yabuch_ch@hotmail.com>
2022-05-05 19:52:07 -0400

    Added background image to the study and quiz cards in the Home page

--- src/Components/QuizMenu/QuizMenu.js
+++ src/Components/QuizMenu/QuizMenu.js
@@ -9,21 +9,21 @@ function QuizMenu(){
 
     return(
         <>
-        <div id="quiz-title">
+        <div id="quiz-title" className="fade-in">
             <h1>Quiz Mode</h1>
             <h3>Test Your Knowledge</h3>
         </div>
         <Container>
         <Row>
             <Col sm={12} md={6}>
-                <div className="quiz-type-card">
+                <div className="quiz-type-card fade-in">
                     <h3>20 Questions</h3>
                     <p>Generates a set of random 20 questions. Answer at least 12 correctly to pass the quiz!</p>
                     <Button href="quiz/twenty-questions">Start This Quiz</Button>
                 </div>
             </Col>
             <Col sm={12} md={6}>
-                <div className="quiz-type-card">
+                <div className="quiz-type-card fade-in">
                     <h3>Coming Soon</h3>
                     <p>New Quiz Modes Coming Soon!</p>
                 </div>
